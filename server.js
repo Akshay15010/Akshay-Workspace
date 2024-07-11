8f0e76aa740997df87c5851a57c9f8116846ee1e
Akshay15010 <mail2akshaypatel18@gmail.com>
2024-07-11 15:07:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 let savedData = '';
 
@@ -17,6 +21,11 @@ app.post('/save', (req, res) => {
 
 app.get('/data', (req, res) => {
     res.json({ data: savedData });
+});
+
+// Serve index.html for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
